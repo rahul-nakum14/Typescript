52f73151850c1ee156b1c7ccb0c074f7229daab9
rahul-nakum14 <106817606+rahul-nakum14@users.noreply.github.com>
2023-12-15 17:40:58 +0530

    Update typescript_learn.ts

--- typescript_learn.ts
+++ typescript_learn.ts
@@ -161,5 +161,138 @@ function createUser123(user: User):User{
 }
 createUser123({name:"sfdfsd",age:123,isPaid:true})
 
+// Readonly and optional
+-----------------------------------
+type User = {
+    readonly id : string;
+    name: string;
+    age: number;
+    isPaid: boolean;
+}
+
+let myuser :User = {
+    id:"thisisid",
+    name :"hacked.com",
+    age:45,
+    isPaid:true
+}
+
+myuser.name = "asdas" //allowd as it is not readonly
+myuser.id = "dsfsf" // not allowd as its readonly
+
+//With Array
+type User1 = {
+  readonly id: string;
+  name: string;
+  age: number;
+  isPaid: boolean;
+  // Prevent mutation with readonly
+  hobbies: readonly string[];
+};
+
+const myUser: User1 = {
+  id: "5678",
+  name: "Jane",
+  age: 25,
+  isPaid: false,
+  hobbies: ["cooking", "music"],
+};
+
+// Cannot add elements to hobbies because it's readonly
+myUser.hobbies.push("dancing"); // Error
 
 
+const numbers: ReadonlyArray<number> = [1, 2, 3];
+
+// Similar error for modifying elements
+numbers.push(4); // Error: Cannot modify a readonly array
+
+// Accessing elements for reading is still allowed
+const sum = numbers.reduce((acc, num) => acc + num, 0); // 6
+
+
+
+// Combinning things 
+----------------------
+type User = {
+    readonly id : string;
+    name: string;
+    age: number;
+    isPaid: boolean;
+    creditcardDetails?:number; // ? allows optional parameter
+}
+
+type creditCardNo = {
+    cardNUmber: number
+}
+
+type cardDate = {
+    cardDate : string
+}
+
+type catdDetails = creditCardNo & cardDate & {
+    cvv : number
+}
+
+
+//Union
+--------------------
+let score : number | string = 44; 
+
+score = 33
+score = "33"
+
+type user = {
+    name: string;
+    age : number;
+}
+
+type admin = {
+    username : string;
+    password: string;
+}
+
+//Shows example that assign two type to the variable rahul and can access property of both user and admin
+let rahul : user | admin = {
+    name:"test",
+    age:43
+}
+
+// Doing with the functions
+function getdbID (id: number | string) {
+    console.log(`ID IS ${id}`)
+}
+getdbID(3)
+getdbID("32")
+
+
+function getdbID1 (id: number | string) {
+    // id.toUpperCase() // Error bcz it is both of type string and number to resolve check type
+    if( typeof (id) === 'string'){
+        id.toUpperCase();
+    }
+    console.log(`ID IS ${id}`)
+}
+
+//Union with the types value
+type alluserdetails = (user | admin) & {
+    data: string;
+}
+
+
+// Working with arrays
+
+const arra1 : number[] = [1,2,23]   //number only
+const arra2 : string[] = ["23","234","234fdd"] // String Only
+
+const arra3 : number[] | string[] = [123,213,432] 
+// Here it means that in array
+// all are number or all are string but [123,213,"432"] not allowed
+
+const arra4 : (number | string)[] = [123,213,432,"23423"] // conain both number and string
+
+
+let seatallotment : "first" | "window" | "middle"
+
+seatallotment = "first" // allowed 
+seatallotment= "crew" // not allowe strict type of only
